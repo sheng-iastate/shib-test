71d6b785ffb4f387936f042a21f1832efc39d88b
sheng <sheng@iastate.edu>
2017-10-10 09:26:16 -0500

    set no cache in the header

--- protected/test.php
+++ protected/test.php
@@ -1,3 +1,11 @@
+<?php
+
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
+?>
 <pre>
 <?php echo $_SERVER['SERVER_ADDR']; ?>
 
