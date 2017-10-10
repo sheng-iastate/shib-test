7a1bffefc5ca62ca431359caf7f5b68b372b34d4
sheng <sheng@iastate.edu>
2017-10-10 06:34:44 -0500

    Clean up test page

--- protected/test.php
+++ protected/test.php
@@ -1,9 +1,9 @@
 <pre>
-<?php echo $_SERVER['SERVER_ADDR ']; ?>
-<?php echo $_SERVER['REMOTE_USER ']; ?>
+<?php echo $_SERVER['SERVER_ADDR']; ?>
+<?php echo $_SERVER['REMOTE_USER']; ?>
 <?php
 
-print_r($_SERVER);
+//print_r($_SERVER);
 
 ?>
 </pre>
